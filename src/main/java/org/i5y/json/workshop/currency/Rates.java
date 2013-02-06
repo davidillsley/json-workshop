0bac8b88698cb228a6116979f17259bba51fe501
David Illsley <david.illsley@digital.cabinet-office.gov.uk>
2013-02-06 12:27:01 +0000

    Cleanup

--- src/main/java/org/i5y/json/workshop/currency/Rates.java
+++ src/main/java/org/i5y/json/workshop/currency/Rates.java
@@ -1,7 +1,7 @@
 package org.i5y.json.workshop.currency;
 
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
@@ -22,12 +22,22 @@ public class Rates {
 		private static final long serialVersionUID = 1L;
 	}
 
-	public double getRate(String currency, String date) {
+	/**
+	 * Get an exchange rate from USD to the provided currency on a certain date
+	 * 
+	 * @param currency
+	 * @param date
+	 *            formatted e.g. 2012-12-01
+	 * @return
+	 * @throws RateNotPresentException
+	 *             if the rate is not available for the currency/date pair
+	 */
+	public static double getRate(String currency, String date) {
 		JsonObject obj = createJsonReader(date).readObject();
 		return getRate(obj, currency);
 	}
 
-	private double getRate(JsonObject jsonDoc, String currency) {
+	private static double getRate(JsonObject jsonDoc, String currency) {
 		JsonObject rates = jsonDoc.getValue("rates", JsonObject.class);
 		if (rates != null) {
 			JsonValue jn = rates.get(currency);
@@ -41,7 +51,12 @@ public class Rates {
 		}
 	}
 
-	public Collection<String> findBaseEquivalentCurrencies(String date) {
+	/**
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static Collection<String> findBaseEquivalentCurrencies(String date) {
 		JsonObject obj = createJsonReader(date).readObject();
 		JsonObject rates = obj.getValue("rates", JsonObject.class);
 		Collection<String> result = new HashSet<>();
@@ -63,16 +78,29 @@ public class Rates {
 	}
 
 	/**
-	 * { currency : "USD", rates: [ {timestamp:"formatted", value: 1.x}, ...] }
+	 * Generates a JSON report for a specific currency for a week from the
+	 * provided date in the format:
+	 * 
+	 * { currency : "USD", rates: [ { timestamp :"Sat Feb 01 23:00:09 GMT 2013",
+	 * value: 1.x}, ...] }
 	 * 
 	 * @param currency
 	 * @param date
 	 * @return
 	 */
-	public JsonObject generateWeekReport(String currency) {
-		List<String> dates = Arrays.asList("2012-12-01", "2012-12-02",
-				"2012-12-03", "2012-12-04", "2012-12-05", "2012-12-06",
-				"2012-12-07");
+	public static JsonObject generateWeekReport(String currency,
+			String startDate) {
+		String[] segments = startDate.split("-");
+		int day = Integer.parseInt(segments[2]);
+
+		List<String> dates = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			String dayStr = Integer.toString(day + i);
+			if (dayStr.length() == 1)
+				dayStr = "0" + dayStr;
+			dates.add(segments[0] + "-" + segments[1] + "-" + dayStr);
+		}
+
 		JsonArrayBuilder ratesArrayBuilder = Json.createArrayBuilder();
 
 		for (String date : dates) {
@@ -91,8 +119,15 @@ public class Rates {
 		return objectBuilder.build();
 	}
 
-	private JsonReader createJsonReader(String date) {
-		InputStream is = getClass().getResourceAsStream("/" + date + ".json");
+	/**
+	 * Return a JsonReader object which will read the document for the
+	 * appropriate date
+	 * 
+	 * @param date
+	 * @return
+	 */
+	private static JsonReader createJsonReader(String date) {
+		InputStream is = Rates.class.getResourceAsStream("/" + date + ".json");
 		return Json.createReader(is);
 	}
 }
--- src/test/java/org/i5y/json/workshop/currency/RatesTest.java
+++ src/test/java/org/i5y/json/workshop/currency/RatesTest.java
@@ -18,12 +18,12 @@ import org.i5y.json.workshop.currency.Rates.RateNotPresentException;
 public class RatesTest extends TestCase {
 
 	public void testGBP20121201() {
-		assertEquals(0.624385, new Rates().getRate("GBP", "2012-12-01"));
+		assertEquals(0.624385, Rates.getRate("GBP", "2012-12-01"));
 	}
 
 	public void testEUR20121202() {
 		try {
-			new Rates().getRate("EUR", "2012-12-02");
+			Rates.getRate("EUR", "2012-12-02");
 			fail("Expected a RateNotPresentException");
 		} catch (RateNotPresentException e) {
 			// expected
@@ -34,36 +34,37 @@ public class RatesTest extends TestCase {
 		Collection<String> expected = new HashSet<>(Arrays.asList("BMD", "BSD",
 				"PAB"));
 		assertEquals(expected,
-				(new Rates().findBaseEquivalentCurrencies("2012-12-01")));
+				(Rates.findBaseEquivalentCurrencies("2012-12-01")));
 	}
 
 	public void testfindBaseEquivalentCurrencies20121202() {
 		Collection<String> expected = new HashSet<>(Arrays.asList("BMD", "BSD",
 				"PAB"));
 		assertEquals(expected,
-				(new Rates().findBaseEquivalentCurrencies("2012-12-02")));
+				(Rates.findBaseEquivalentCurrencies("2012-12-02")));
 	}
 
-	public void testGenerateReport(){
+	public void testGenerateReport() {
 		Map<String, Double> expectedRates = new HashMap<>();
-		expectedRates.put("Sat Dec 01 23:00:09 GMT 2012",0.624385);
-		expectedRates.put("Sun Dec 02 23:00:08 GMT 2012",0.624168);
-		expectedRates.put("Mon Dec 03 23:00:08 GMT 2012",0.62174);
-		expectedRates.put("Tue Dec 04 23:00:08 GMT 2012",0.620952);
-		expectedRates.put("Wed Dec 05 23:00:08 GMT 2012",0.621273);
-		expectedRates.put("Thu Dec 06 23:00:08 GMT 2012",0.62268);
-		expectedRates.put("Fri Dec 07 23:00:08 GMT 2012",0.62287);
-		JsonObject value = new Rates().generateWeekReport("GBP");
+		expectedRates.put("Sat Dec 01 23:00:09 GMT 2012", 0.624385);
+		expectedRates.put("Sun Dec 02 23:00:08 GMT 2012", 0.624168);
+		expectedRates.put("Mon Dec 03 23:00:08 GMT 2012", 0.62174);
+		expectedRates.put("Tue Dec 04 23:00:08 GMT 2012", 0.620952);
+		expectedRates.put("Wed Dec 05 23:00:08 GMT 2012", 0.621273);
+		expectedRates.put("Thu Dec 06 23:00:08 GMT 2012", 0.62268);
+		expectedRates.put("Fri Dec 07 23:00:08 GMT 2012", 0.62287);
+		JsonObject value = Rates.generateWeekReport("GBP", "2012-12-01");
 		assertEquals("GBP", value.getStringValue("currency"));
-		
+
 		JsonArray rates = value.getValue("rates", JsonArray.class);
 		assertEquals(7, rates.size());
-		
-		for(JsonValue jv: rates){
+
+		for (JsonValue jv : rates) {
 			assertTrue(jv instanceof JsonObject);
-			JsonObject jo = (JsonObject)jv;
+			JsonObject jo = (JsonObject) jv;
 			String timestamp = jo.getStringValue("timestamp");
-			double rate = jo.getValue("rate", JsonNumber.class).getDoubleValue();
+			double rate = jo.getValue("rate", JsonNumber.class)
+					.getDoubleValue();
 			assertEquals(expectedRates.get(timestamp), rate);
 		}
 	}
